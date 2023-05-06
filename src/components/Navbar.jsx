56fa40f20a819f96946b1b9b946ffd6fe880e610
thatchermain <dominikkrzysztalowicz@gmail.com>
2023-05-06 13:47:43 +0200

    Fully Responsive home page with mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Navbar.scss";
 import Logo from "../assets/images/logo.png";
 import { Link } from "react-router-dom";
 import { TfiFacebook } from "react-icons/tfi";
 import { BsInstagram } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { GrClose } from "react-icons/gr";
 
 const Navbar = () => {
+  const [showMobile, setShowMobile] = useState(false);
+
   return (
     <div className="navbar_fixed">
       <div className="navbar_container">
@@ -14,28 +18,69 @@ const Navbar = () => {
             <img src={Logo} alt="" />
           </div>
         </Link>
+        {/* <div className={`navbar_menu ${isOpen ? "visible" : "hidden"}`}> */}
         <div className="navbar_menu">
-          <ul className="navbar_menu__list">
-            <Link className="navbar_menu__item" to="/oferta">
-              OFERTA
+          {/* <ul className="navbar_menu__list "> */}
+          <ul
+            className={`navbar_menu__list ${
+              showMobile ? "navbar_menu__list visible" : "navbar_menu__list"
+            }
+          `}
+          >
+            <div
+              className="mobile-nav-close-btn"
+              onClick={() => setShowMobile(false)}
+            >
+              <GrClose />
+            </div>
+            <Link
+              className="navbar_menu__item"
+              to="/oferta"
+              onClick={() => setShowMobile(false)}
+            >
+              Oferta
             </Link>
-            <Link className="navbar_menu__item" to="/dystrybucja">
-              DYSTRYBUCJA
+            <Link
+              className="navbar_menu__item"
+              to="/dystrybucja"
+              onClick={() => setShowMobile(false)}
+            >
+              Dystrybucja
             </Link>
-            <Link className="navbar_menu__item" to="/oferty_pracy">
-              OFERTY PRACY
+            <Link
+              className="navbar_menu__item"
+              to="/oferty_pracy"
+              onClick={() => setShowMobile(false)}
+            >
+              Oferty pracy
             </Link>
-            <Link className="navbar_menu__item" to="/realizacje">
-              REALIZACJE
+            <Link
+              className="navbar_menu__item"
+              to="/realizacje"
+              onClick={() => setShowMobile(false)}
+            >
+              Realizacje
             </Link>
-            <Link className="navbar_menu__item" to="/blog">
-              BLOG
+            <Link
+              className="navbar_menu__item"
+              to="/blog"
+              onClick={() => setShowMobile(false)}
+            >
+              Blog
             </Link>
-            <Link className="navbar_menu__item" to="/do_pobrania">
-              DO POBRANIA
+            <Link
+              className="navbar_menu__item"
+              to="/do_pobrania"
+              onClick={() => setShowMobile(false)}
+            >
+              Do pobrania
             </Link>
-            <Link className="navbar_menu__item" to="/kontakt">
-              KONTAKT
+            <Link
+              className="navbar_menu__item"
+              to="/kontakt"
+              onClick={() => setShowMobile(false)}
+            >
+              Kontakt
             </Link>
           </ul>
         </div>
@@ -56,6 +101,9 @@ const Navbar = () => {
           >
             <BsInstagram className="navbar_social__icon" />
           </Link>
+        </div>
+        <div className="mobile-nav-btn" onClick={() => setShowMobile(true)}>
+          <HiOutlineBars3 />
         </div>
       </div>
     </div>
